77d05aae15804d6ba2a284f59f76f3ebeeefb988
erjona505 <erjona.kalari@gmail.com>
2026-01-12 04:19:27 +0000

    update configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig({
-  base: "/personal-website/", 
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/personal-website/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
-
-
+}));
